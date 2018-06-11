e3c324d6903979fe8cb4f208967fbb1002de5a4d
YaushiOkamura <okamura@net.lab.uec.ac.jp>
2018-06-11 14:28:00 +0900

    仮の学生テーブルのCRUD実装

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,46 @@
 class StudentsController < ApplicationController
-  def index
+  before_action :set_student, only: [:edit, :update, :destroy]
+ 
+ def index
     @students = Student.all
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def edit
+  end
+
+  def create
+    @student = Student.create(student_params)
+    if @student.save
+      redirect_to students_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @student.update(student_params)
+      redirect_to students_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @student.destroy
+    redirect_to students_path
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :result, :mother_existance, :parent_income)
+  end
+
+  def set_student
+    @student = Student.find(params[:id])
   end
 end
