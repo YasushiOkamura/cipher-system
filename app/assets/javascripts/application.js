6cb14e1f8212820d121620cbcb5fb08f1a35da35
Yasushi Okamura <okamura@net.lab.uec.ac.jp>
2018-07-23 12:00:27 +0900

    見た目の調整

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,10 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require jquery
+//= require jquery_ujs
+//= require rails-ujs
+//= require turbolinks
+// Loads all Semantic javascripts
+//= require semantic-ui
+//= require_tree .
